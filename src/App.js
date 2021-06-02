6150147eb23ce2fe4887b85693ca75c7726b8c20
anjant-uptycs <anjant@uptycs.com>
2021-06-02 12:26:24 +0530

    6.16 Adding Routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
 import Movies from "./components/movies";
+import { Redirect, Route, Switch } from "react-router-dom";
+import Rentals from "./components/rentals";
+import Customers from "./components/customers";
+import NotFound from "./components/notFound";
+
 function App() {
   return (
     <main className="container">
-      <Movies />
+      <Switch>
+        <Route path="/movies" component={Movies}></Route>
+        <Route path="/customers" component={Customers}></Route>
+        <Route path="/rentals" component={Rentals}></Route>
+        <Route path="/not-found" component={NotFound}></Route>
+        <Redirect exact from="/" to="/movies" />
+        <Redirect to="/not-found" />
+      </Switch>
     </main>
   );
 }
