fb05743a6055bee6370557888bf6876d510d7bb9
anjant-uptycs <anjant@uptycs.com>
2021-06-04 14:40:26 +0530

    Add routing to Vidly

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import "./App.css";
+import React from "react";
 import Movies from "./components/movies";
 import { Redirect, Route, Switch } from "react-router-dom";
 import Rentals from "./components/rentals";
 import Customers from "./components/customers";
 import NotFound from "./components/notFound";
-
+import NavBar from "./components/navbar";
+import MovieForm from "./components/movieForm";
 function App() {
   return (
-    <main className="container">
-      <Switch>
-        <Route path="/movies" component={Movies}></Route>
-        <Route path="/customers" component={Customers}></Route>
-        <Route path="/rentals" component={Rentals}></Route>
-        <Route path="/not-found" component={NotFound}></Route>
-        <Redirect exact from="/" to="/movies" />
-        <Redirect to="/not-found" />
-      </Switch>
-    </main>
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/movies/:id" component={MovieForm}></Route>
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/customers" component={Customers}></Route>
+          <Route path="/rentals" component={Rentals}></Route>
+          <Route path="/not-found" component={NotFound}></Route>
+          <Redirect exact from="/" to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
