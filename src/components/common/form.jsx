f70aced3127a491ee87972f1e2dd113e882af409
Surya Prakash Anjan Talatam <tspa2117@gmail.com>
2021-08-10 16:38:34 +0530

    7.17, 7.18 Extracting a reusable form

---
+++ src/components/common/form.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import Joi from "joi-browser";
+
+class Form extends Component {
+  validate = () => {
+    const errors = {};
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(this.state.data, this.schema, options);
+    error &&
+      error.details.forEach((error) => (errors[error.path[0]] = error.message));
+    return Object.keys(errors).length !== 0 ? errors : null;
+  };
+
+  validateProperty = ({ name, value }) => {
+    const obj = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(obj, schema, { abortEarly: false });
+    return error ? error.details[0].message : null;
+  };
+
+  handleChange = ({ target: input }) => {
+    const errors = { ...this.state.errors };
+    const data = { ...this.state.data };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+    data[input.name] = input.value;
+    this.setState({ data, errors });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const errors = this.validate() || {};
+    this.setState({ errors });
+    if (Object.keys(errors).length > 0) return;
+
+    this.doSubmit();
+  };
+}
+
+export default Form;
--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,10 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
 import Input from "./common/input";
 import Joi from "joi-browser";
+import Form from "./common/form";
 
-class LoginForm extends Component {
+class LoginForm extends Form {
   state = {
-    account: { username: "", password: "" },
+    data: { username: "", password: "" },
     errors: {},
   };
 
@@ -13,58 +14,28 @@ class LoginForm extends Component {
     password: Joi.string().required().label("Password"),
   };
 
-  validate = () => {
-    const errors = {};
-    const options = { abortEarly: false };
-    const { error } = Joi.validate(this.state.account, this.schema, options);
-    error &&
-      error.details.forEach((error) => (errors[error.path[0]] = error.message));
-    return Object.keys(errors).length !== 0 ? errors : null;
-  };
-
-  validateProperty = ({ name, value }) => {
-    const obj = { [name]: value };
-    const schema = { [name]: this.schema[name] };
-    const { error } = Joi.validate(obj, schema, { abortEarly: false });
-    return error ? error.details[0].message : null;
-  };
-
-  handleChange = ({ target: input }) => {
-    const errors = { ...this.state.errors };
-    const account = { ...this.state.account };
-    const errorMessage = this.validateProperty(input);
-    if (errorMessage) errors[input.name] = errorMessage;
-    else delete errors[input.name];
-    account[input.name] = input.value;
-    this.setState({ account, errors });
-  };
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    const errors = this.validate() || {};
-    this.setState({ errors });
-    if (errors) return;
-
+  doSubmit = () => {
     // Code that goes to server
     console.log("Form Submitted");
   };
+
   render() {
-    const { account, errors } = this.state;
-    console.log(errors);
+    const { data, errors } = this.state;
+
     return (
       <div>
         <h1>Login Form </h1>
         <form onSubmit={this.handleSubmit}>
           <Input
             name='username'
-            value={account.username}
+            value={data.username}
             label='Username'
             onChange={this.handleChange}
             error={errors.username}
           />
           <Input
             name='password'
-            value={account.password}
+            value={data.password}
             label='Password'
             onChange={this.handleChange}
             error={errors.password}
